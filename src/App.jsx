44202112e0b059d6b91de2d2fad244de735c9b87
amletodellaloggia <amletodellaloggia@gmail.com>
2025-09-01 13:35:07 +0200

    [upd] imported useState, useEffect, added select div, set option values

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-function App() {
+import { useEffect, useState } from "react";
 
+const moviesList =  [
+   { title: 'Inception', genre: 'Fantascienza' },
+   { title: 'Il Padrino', genre: 'Thriller' },
+   { title: 'Titanic', genre: 'Romantico' },
+   { title: 'Batman', genre: 'Azione' },
+   { title: 'Interstellar', genre: 'Fantascienza' },
+   { title: 'Pulp Fiction', genre: 'Thriller' },
+ ];
+
+function App() {
   return (
     <>
-    <h2>Hello world</h2>
+    <div className="container">
+      <select >
+        <option value="">Seleziona Genere</option>
+        <option value="Fantascienza">Fantascienza</option>
+        <option value="Thriller">Thriller</option>
+        <option value="Romantico">Romantico</option>
+        <option value="Azione">Azione</option>
+      </select>
+    </div>
     </>
   )
 }
